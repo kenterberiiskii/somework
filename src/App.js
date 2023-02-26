67eb8825536560dd795eb057924e94b0687cb42f
kenterberiiskii <a.mizyakov@gmail.com>
2023-02-26 10:08:13 +0300

    work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import UserList from './Components/User.js'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+            this.state = {
+                'users': []
+            }
+    }
+//    componentDidMount() {
+//        const users = [
+//            {
+//            'first_name': 'Фёдор',
+//            'last_name': 'Достоевский',
+//            'username': 1821
+//            },
+//            {
+//            'first_name': 'Александр',
+//            'last_name': 'Грин',
+//            'username': 1880
+//            },
+//        ]
+//        this.setState(
+//            {
+//            'users': users
+//            }
+//        )
+//    }
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/users')
+        .then(response => {
+        const users = response.data
+        this.setState(
+        {
+        'users': users
+        }
+        )
+        }).catch(error => console.log(error))
+    }
+
+    render () {
+        return (
+            <div>
+            <UserList users={this.state.users} />
+            </div>
+        )
+    }
 }
 
 export default App;
